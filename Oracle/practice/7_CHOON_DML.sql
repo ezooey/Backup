9dbc53bb86ad9f04c49188ef801e1eee3b88099d
ezooey <ezooey0@gmail.com>
2021-09-19 11:17:29 +0900

    210919 CHOON DML

--- Oracle/practice/7_CHOON_DML.sql
+++ Oracle/practice/7_CHOON_DML.sql
@@ -24,9 +24,28 @@ UPDATE TB_DEPARTMENT
 SET CAPACITY = ROUND(CAPACITY*1.1);
 
 -- 5. 학번 A413042인 박건우 학생의 주소를 "서울시 종로구 숭인동 181-21"로 정정
+UPDATE TB_STUDENT
+SET STUDENT_ADDRESS = '서울시 종로구 숭인동 181-21'
+WHERE STUDENT_NO = 'A413042';
 
 -- 6. 학생정보 테이블에서 주민번호 뒷자리를 저장하지 않도록 수정
+UPDATE TB_STUDENT
+SET STUDENT_SSN = SUBSTR(STUDENT_SSN, 1, 6);
 
 -- 7. 의학과 김명훈 학생이 2005년 1학기에 수강한 '피부생리학' 점수의 학점을 3.5로 변경
+UPDATE TB_GRADE
+SET POINT = 3.5
+WHERE STUDENT_NO = (SELECT STUDENT_NO
+                    FROM TB_STUDENT
+                         JOIN TB_DEPARTMENT USING(DEPARTMENT_NO)
+                    WHERE STUDENT_NAME = '김명훈' AND DEPARTMENT_NAME = '의학과') 
+      AND CLASS_NO = (SELECT CLASS_NO
+                      FROM TB_CLASS
+                      WHERE CLASS_NAME = '피부생리학')
+      AND TERM_NO = 200501;
 
 -- 8. 성적 테이블(TB_GRADE)에서 휴학생들의 성적항목 제거
+DELETE FROM TB_GRADE
+WHERE STUDENT_NO IN (SELECT STUDENT_NO
+                    FROM TB_STUDENT
+                    WHERE ABSENCE_YN = 'Y');
