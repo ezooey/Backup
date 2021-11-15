eddf63b62ec5cc808260924d6df980ef90415930
ezooey <ezooey0@gmail.com>
2021-11-15 19:09:00 +0900

    211115 비밀번호 찾기 추가

---
+++ Semi-Project/0_Shinwoo/src/member/dao/MemberDAO.java
@@ -0,0 +1,188 @@
+package member.dao;
+import static common.JDBCTemplate.*;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import member.vo.Member;
+
+public class MemberDAO {
+	private Properties prop = null;
+	
+	public MemberDAO() {
+		prop = new Properties();
+		
+		String fileName = MemberDAO.class.getResource("/sql/member/member-query.properties").getPath();
+		
+		try {
+			prop.load(new FileReader(fileName));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Member loginMember(String memberId, String memberPwd, Connection conn) {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		Member loginUser = null;
+		
+		String query = prop.getProperty("login");
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, memberId);
+			pstmt.setString(2, memberPwd);
+			
+			rset = pstmt.executeQuery();
+			
+			if(rset.next()) {
+				loginUser = new Member(rset.getString("MEMBER_ID"),
+									   rset.getString("PW"),
+									   rset.getString("MEMBER_NAME"),
+									   rset.getString("DEPARTMENT"),
+									   rset.getString("PHONE"),
+									   rset.getString("ADDRESS"),
+									   rset.getString("EMAIL"),
+									   rset.getString("MEMBER_TYPE"),
+									   rset.getString("STATUS"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		return loginUser;
+		
+		
+	}
+
+	public String isFirstLogin(String memberId, Connection conn) {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String status = null;
+		
+		String query = prop.getProperty("checkFirst");
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, memberId);
+			
+			rset = pstmt.executeQuery();
+			
+			if(rset.next()) {
+				status = rset.getString(1);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		return status;
+		
+		
+	}
+
+	public int firstUpdate(Connection conn, Member m) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		String query = prop.getProperty("firstUpdate");
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, m.getMemberPwd());
+			pstmt.setString(2, m.getPhone());
+			pstmt.setString(3, m.getAddress());
+			pstmt.setString(4, m.getEmail());
+			pstmt.setString(5, m.getMemberId());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		
+		return result;
+	}
+	
+	public int updatePwd(Connection conn, Member m) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		String query = prop.getProperty("updatePwd");
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			
+			pstmt.setString(1, m.getMemberPwd());
+			pstmt.setString(2, m.getMemberId());
+			
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		
+		return result;
+	}
+
+	public int updateMember(Connection conn, Member m) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		String query = prop.getProperty("updateMember");
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			
+			pstmt.setString(1, m.getPhone());
+			pstmt.setString(2, m.getAddress());
+			pstmt.setString(3, m.getEmail());
+			pstmt.setString(4, m.getMemberId());
+			
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		return result;
+	}
+
+	public String findPwd(Connection conn, String memberId) {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String email = null;
+		
+		String query = prop.getProperty("findPwd");
+		// findPwd=SELECT EMAIL FROM MEMBER WHERE MEMBER_ID = ?
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, memberId);
+			
+			rset = pstmt.executeQuery();
+			if(rset.next()) {
+				email = rset.getString("email");
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		return email;
+	}
+
+}
---
+++ Semi-Project/0_Shinwoo/src/member/service/MemberService.java
@@ -0,0 +1,94 @@
+package member.service;
+
+import java.sql.Connection;
+import static common.JDBCTemplate.*;
+import member.dao.MemberDAO;
+import member.vo.Member;
+
+public class MemberService {
+	private MemberDAO mDAO = new MemberDAO();
+
+	public Member loginMember(String memberId, String memberPwd) {
+		Connection conn = getConnection();
+		
+		Member loginUser = mDAO.loginMember(memberId, memberPwd, conn);
+		
+		close(conn);
+		
+		return loginUser;
+		
+	}
+
+	public String isFirstLogin(String memberId) {
+		Connection conn = getConnection();
+		
+		String status = mDAO.isFirstLogin(memberId, conn);
+		
+		close(conn);
+		return status;
+	}
+
+	public int firstUpdate(Member m) {
+		Connection conn = getConnection();
+		
+		int result = mDAO.firstUpdate(conn, m);
+		
+		if(result > 0) {
+			commit(conn);
+		}else {
+			rollback(conn);
+		}
+		
+		close(conn);
+		
+		return result;
+	}
+		
+	public int updateMember(Member m) {
+		Connection conn = getConnection();
+		
+		int result = mDAO.updateMember(conn, m);
+		
+		if(result > 0) {
+			commit(conn);
+		} else {
+			rollback(conn);
+		}
+		
+		close(conn);
+		
+		return result;
+	}
+
+	public int updatePwd(Member m) {
+		Connection conn = getConnection();
+		
+		int result = mDAO.updatePwd(conn, m);
+		
+		if(result > 0) {
+			commit(conn);
+		} else {
+			rollback(conn);
+		}
+		
+		close(conn);
+		
+		return result;
+	}
+
+	public String findPwd(String memberId) {
+		Connection conn = getConnection();
+		String email = mDAO.findPwd(conn, memberId);
+		
+		if(email != null) {
+			commit(conn);
+		} else {
+			rollback(conn);
+		}
+		close(conn);
+		return email;
+	}
+	
+	
+
+}
